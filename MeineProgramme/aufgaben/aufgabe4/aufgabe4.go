b62103f97f474f567a8f0171e5328d3577951c39
Anes Sahbegovic <anes.sahbegovic@gmail.com>
2025-12-04 17:08:55 +0100

    aufgabe 4

--- MeineProgramme/aufgaben/aufgabe4/aufgabe4.go
+++ MeineProgramme/aufgaben/aufgabe4/aufgabe4.go
@@ -13,8 +13,46 @@ package aufgabe4
 // hinteren Werte ihr letztes Element verwendet werden.
 // Für leere Listen soll für die Berechnung ggf. 0 verwendet werden.
 func ElementSums(l1, l2 []int) []int {
+	//Beispiel l1 = 5 und l3 =3
 	result := []int{}
+	var diff int
+	var max int
+	if len(l1) == len(l2) {
+		for i := 0; i < len(l1); i++ {
+			x := l1[i] + l2[i]
+			result = append(result, x)
+		}
+		return result
+	}
+
+	if len(l1) < len(l2) {
+		diff = len(l2) - len(l1)
+		max = len(l1)
+		for j := 0; j < max; j++ {
+			y := l1[j] + l2[j]
+			result = append(result, y)
+		}
+		for k := 0; k < diff; k++ {
+			z := l1[max-1] + l2[max+k]
+			result = append(result, z)
+		}
+	}
+
+	if len(l1) > len(l2) {
+		diff = len(l1) - len(l2)
+		max = len(l2)
+
+		for j := 0; j < max; j++ {
+			y := l2[j] + l1[j]
+			result = append(result, y)
+		}
+		for k := 0; k < diff; k++ {
+			z := l2[max-1] + l1[max+k]
+			result = append(result, z)
+		}
+	}
+	// diff = 2
+	//max = 3
+
 	return result
 }
-
-//nicht geschafft
